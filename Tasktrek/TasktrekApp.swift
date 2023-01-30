c311a1d24a170600e4136d66dd3dc61cf87ab912
swiftcynic <dhruv.salot@outlook.com>
2023-01-30 12:45:22 +0530

    Initial Commit

---
+++ Tasktrek/TasktrekApp.swift
@@ -0,0 +1,17 @@
+//
+//  TasktrekApp.swift
+//  Tasktrek
+//
+//  Created by Dhruv Salot on 30/01/23.
+//
+
+import SwiftUI
+
+@main
+struct TasktrekApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
